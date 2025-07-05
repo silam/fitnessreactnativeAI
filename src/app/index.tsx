d0b034c1f7c9f1753b20d63091a045882a9a6a8c
Si Lam <silam@hotmail.com>
2025-07-05 10:28:30 -0500

    Cleanup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -26,43 +26,9 @@ function Content() {
 
             <Text className="mx-auto max-w-[700px] text-lg text-center md:text-xl">
               This template sets up Expo and Tailwind (NativeWind) allowing you
-              to quickly get started with my YouTube tutorial!
+              to quickly get started!
             </Text>
-            <Link href="https://www.youtube.com/@sonnysangha" target="_blank">
-              <Text className="text-lg text-center text-blue-500 hover:text-blue-700 underline md:text-xl dark:text-blue-400 dark:hover:text-blue-300">
-                https://www.youtube.com/@sonnysangha
-              </Text>
-            </Link>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="https://www.youtube.com/@sonnysangha"
-              >
-                Visit my YouTube Channel
-              </Link>
-            </View>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-red-700 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="https://www.papareact.com/course"
-              >
-                Get the Complete Source Code (Plus 60+ builds) ❤️
-              </Link>
-            </View>
-
-            <View className="gap-4">
-              <Link
-                suppressHighlighting
-                className="flex h-9 items-center justify-center overflow-hidden rounded-md bg-green-700 px-4 py-2 text-sm font-medium text-gray-50 web:shadow ios:shadow transition-colors hover:bg-gray-900/90 active:bg-gray-400/90 web:focus-visible:outline-none web:focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                href="https://www.papareact.com/course"
-              >
-                Join My Course & Learn to Code with AI 💚 (1000+ Students)
-              </Link>
-            </View>
+          
           </View>
         </View>
       </View>
@@ -75,14 +41,14 @@ function Header() {
     <View>
       <View className="px-4 lg:px-6 h-14 flex items-center flex-row justify-between ">
         <Link className="font-bold flex-1 items-center justify-center" href="/">
-          PAPAFAM
+          Fitness Tracker
         </Link>
         <View className="">
           <Link
             className="text-md font-medium hover:underline web:underline-offset-4"
-            href="https://www.papareact.com/course"
+            href="https://serverlessdeveloper.com"
           >
-            Join My Course ❤️
+            Fitness Traker
           </Link>
         </View>
       </View>
